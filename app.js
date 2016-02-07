dd495b5984b7562d984a4de59533e3a159c1d9a2
Justin Castilla <justincastilla@users.noreply.github.com>
2016-02-07 14:17:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,81 @@
-var app = angular.module('movieApp', []);
+angular.module('movieApp',[])
+  .controller('MovieCtrl', MovieCtrl);
 
-app.controller('MovieCtrl', ['$scope', function ($scope) {
-  $scope.moviesToWatch = [
-    {
-      title: 'Inside Out',
-      image: 'http://blogs-images.forbes.com/markhughes/files/2015/06/INSIDE-OUT-18.jpg'
-    },
-    {
-      title: 'Star Wars: Episode VII',
-      image: 'http://cdn.wegotthiscovered.com/wp-content/uploads/star-wars-episode-72.jpg'
-    },
-    {
-      title: 'Shaun the Sheep',
-      image: 'https://silentlondon.files.wordpress.com/2015/01/shaun_chase_quad_uk-release-date.jpg'
-    },
-    {
-      title: 'The Big Short',
-      image: 'http://static.standard.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2015/09/22/17/rysangos.jpg'
-    },
-    {
-      title: 'Steve Jobs',
-      image: 'http://www.geek.com/wp-content/uploads/2015/10/steve-jobs-movie-2015-holding.jpg'
-    },
-    {
-      title: 'Trainwreck',
-      image: 'http://www.trbimg.com/img-55a53d5c/turbine/ct-trainwreck-movie-review-amy-schumer-20150713'
+
+  function MovieCtrl() {
+    console.log("Movie Controller active!");
+    var vm = this;
+
+    vm.moviesToWatch = [
+      {
+        title: 'Inside Out',
+        image: 'http://blogs-images.forbes.com/markhughes/files/2015/06/INSIDE-OUT-18.jpg'
+      },
+      {
+        title: 'Star Wars: Episode VII',
+        image: 'http://cdn.wegotthiscovered.com/wp-content/uploads/star-wars-episode-72.jpg'
+      },
+      {
+        title: 'Shaun the Sheep',
+        image: 'https://silentlondon.files.wordpress.com/2015/01/shaun_chase_quad_uk-release-date.jpg'
+      },
+      {
+        title: 'The Big Short',
+        image: 'http://static.standard.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2015/09/22/17/rysangos.jpg'
+      },
+      {
+        title: 'Steve Jobs',
+        image: 'http://www.geek.com/wp-content/uploads/2015/10/steve-jobs-movie-2015-holding.jpg'
+      },
+      {
+        title: 'Se7en',
+        image: 'http://cromeyellow.com/wp-content/uploads/2014/06/brad-pitt_se7en-crying.jpg'
+      },
+      {
+        title: 'Trainwreck',
+        image: 'http://www.trbimg.com/img-55a53d5c/turbine/ct-trainwreck-movie-review-amy-schumer-20150713'
+      }
+    ];
+
+    // clear any previous movie attribute in vm.movie
+    vm.movie = {};
+    vm.addMovie = function () {
+      var newMovie = vm.movie;
+      vm.movie = {};
+      vm.moviesToWatch.push(newMovie);
+    };
+
+    vm.deleteMovie = function (movie) {
+      var movieIndex = vm.moviesToWatch.indexOf(movie);
+      vm.moviesToWatch .splice(movieIndex, 1);
+    };
+
+    vm.markWatched = function (movie) {
+      movie.watched = (movie.watched ? false : true);
     }
-  ];
 
-  $scope.movie = {};
-  $scope.addMovie = function() {
-    var newMovie = $scope.movie;
-    $scope.movie = {};
-    $scope.moviesToWatch.push(newMovie);
-  };
+    vm.movieLimit = 5;
+    vm.toggleMovieLimit = function() {
+      if (vm.movieLimit) {
+        vm.movieLimit = false;
+      } else {
+        vm.movieLimit = 5;
+      }
+    };
 
-  $scope.deleteMovie = function (movie) {
-    var movieIndex = $scope.moviesToWatch.indexOf(movie);
-    $scope.moviesToWatch.splice(movieIndex, 1);
-  };
+    vm.changeBackground = function() {
+      var randomIndex = Math.floor(Math.random() * vm.moviesToWatch.length);
+      console.log(randomIndex);
+      randomImage = vm.moviesToWatch[randomIndex].image;
+      vm.style = {'background-image': 'url(' + randomImage + ')'};
+    };
 
-  $scope.markWatched = function (movie) {
-    movie.watched = (movie.watched ? false : true);
-  };
-
-  $scope.movieLimit = 5;
-  $scope.toggleMovieLimit = function() {
-    if ($scope.movieLimit) {
-      $scope.movieLimit = false;
-    } else {
-      $scope.movieLimit = 5;
+    vm.clearBackground =  function() {
+      vm.style = {'background-image': 'none'};
     }
+    
+    // friendly object inspection in browser
+    console.log(vm);
   };
 
-  $scope.changeBackground = function() {
-    randomIndex = Math.floor(Math.random() * $scope.moviesToWatch.length);
-    console.log(randomIndex);
-    randomImage = $scope.moviesToWatch[randomIndex].image;
-    $scope.style = {'background-image': 'url(' + randomImage + ')'};
-  };
-
-  $scope.clearBackground = function() {
-    $scope.style = {'background-image': 'none'};
-  };
-
-}]);
+console.log("app.js loaded!");
