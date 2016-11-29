9e2e2d3b2fd101e9984421f7acd8af168cd921b7
Vladyslav Kuliani <vladyslavkuliani@Vladyslavs-MacBook-Pro.local>
2016-11-29 09:43:45 -0800

    bonus part

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 angular.module('movieApp', []).controller('MovieController', MovieController);
 
+var pictures = ['http://wallpapercave.com/wp/QlF13TB.jpg', 'https://static1.squarespace.com/static/56ff005dc6fc084cc7e651a7/t/56ff91261bbee0d82526fee3/1459657124842/amazing-sun-rays-through-golden-gate-bridge-hd-wallpaper-574472.jpg?format=2500w', 'http://wallpapercave.com/wp/1lS7grH.jpg'];
+
 function MovieController(){
 
   var vm = this;
@@ -8,22 +10,26 @@ function MovieController(){
     {
       id: 1,
       title: "Lord of the Rings",
-      genre: "fantasy"
+      genre: "fantasy",
+      moviePicture: "../public/images/defaultPic.jpg"
     },
     {
       id: 2,
       title: "Shooter",
-      genre: "action"
+      genre: "action",
+      moviePicture: "../public/images/defaultPic.jpg"
     },
     {
       id: 3,
       title: "Shrek",
-      genre: "cartoon"
+      genre: "cartoon",
+      moviePicture: "../public/images/defaultPic.jpg"
     },
     {
       id: 4,
       title: "Bourne",
-      genre: "action"
+      genre: "action",
+      moviePicture: "../public/images/defaultPic.jpg"
     }
   ];
 
@@ -34,5 +40,12 @@ function MovieController(){
   vm.deleteMovie = function(movie){
     var index = vm.moviesToWatch.indexOf(movie);
     vm.moviesToWatch.splice(index, 1);
+  }
+
+  vm.randomPic = function(movie){
+    var random = Math.random()*(pictures.length-1);
+    random = Math.round(random);
+
+    movie.moviePicture = pictures[random];
   }
 }
